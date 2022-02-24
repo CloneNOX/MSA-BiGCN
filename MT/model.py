181e73cd9206a41ccfb55f9a0cb3e26656478c65
CloneNOX <xsr@LAPTOP-G9MLGIT2.localdomain>
2022-02-24 22:29:21 +0800

    update MT

--- MT/model.py
+++ MT/model.py
@@ -3,8 +3,8 @@ import torch.nn as nn
 from torch.nn.modules.module import Module
 
 class MTUS(nn.Module):
-    def __init__(self, embeddingDim: int, hiddenDim: int, inputDim: int, 
-                 numGRULayer: int, numRumorClass: int, numStanceClass: int,
+    def __init__(self, inputDim: int, numRumorClass: int, numStanceClass: int,  
+                 numGRULayer=2, embeddingDim=100, hiddenDim=100,
                  batchSize = 1, bidirectional = False):
         super().__init__() # 调用nn.Moudle父类的初始化方法
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu') # 优先使用cuda
@@ -40,9 +40,9 @@ class MTUS(nn.Module):
     def forwardStance(self, sentences: torch.Tensor):
         seqLen = sentences.size()[0]
         embeddings = self.embeddingRumor(sentences).view(seqLen, self.batchSize, self.embeddingDim)
-        gruOut, _ = self.shareGRU(embeddings, self.h0) # hs(seqLen, batch, numDirection * hiddenDim)
-        h1Repeat = gruOut[0].repeat(seqLen, 1, 1) # h1Repeat(seqLen, batch, numDirection * hiddenDim)
-        p = self.vStance(torch.cat([h1Repeat, gruOut], dim=2))
+        gruOut, _ = self.shareGRU(embeddings, self.h0) # gruOut(seqLen, batch, numDirection * hiddenDim)
+        h1 = gruOut[0].repeat(seqLen, 1, 1) # h1(seqLen, batch, numDirection * hiddenDim)
+        p = self.vStance(torch.cat([h1, gruOut], dim=2))
         return p
 
     # 更换计算设备
@@ -59,40 +59,51 @@ class MTUS(nn.Module):
         self.load_state_dict(torch.load(path))
     
 class MTES(nn.Module):
-    def __init__(self, embeddingDim: int, hiddenDim: int, inputDim: int, numGRULayer: int, numRumorClass: int, numStanceClass: int, batchSize=1):
+    def __init__(self, inputDim: int, numRumorClass: int, numStanceClass: int,  
+                 numGRULayer=2, embeddingDim=100, hiddenDim=100,
+                 batchSize = 1, bidirectional = False):
         super().__init__() # 调用nn.Moudle父类的初始化方法
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu') # 优先使用cuda
         self.embeddingDim = embeddingDim
         self.hiddenDim = hiddenDim
         self.batchSize = batchSize
+        self.bidirectional = bidirectional
+        self.D = 2 if self.bidirectional else 1
         
         # embedding使用线性层来把tf-idf向量转换成句嵌入
         self.embeddingRumor = nn.Linear(inputDim, embeddingDim)
         self.embeddingStance = nn.Linear(inputDim, embeddingDim)
+
         # 共享GRU层
-        self.shareGRU = nn.GRU(embeddingDim, hiddenDim, numGRULayer)
-        self.h0 = nn.Parameter(torch.randn((numGRULayer, batchSize, hiddenDim)))
+        self.GRUshare = nn.GRU(embeddingDim, hiddenDim, numGRULayer)
+        self.h0Share = nn.Parameter(torch.randn((numGRULayer, batchSize, hiddenDim)))
+
         # 非共享GRU
         self.GRURumor = nn.GRU(hiddenDim, hiddenDim, numGRULayer)
         self.GRUStance = nn.GRU(hiddenDim, hiddenDim, numGRULayer)
         self.h0Rumor = nn.Parameter(torch.randn((numGRULayer, batchSize, hiddenDim)))
         self.h0Stance = nn.Parameter(torch.randn((numGRULayer, batchSize, hiddenDim)))
+
         # 共享GRU隐状态接入非共享层的系数
-        self.u = nn.Linear(hiddenDim, embeddingDim)
+        self.US2M = nn.Linear(self.D * hiddenDim, embeddingDim)
+
         # 把GRU的隐状态映射成概率
-        self.vRumor = nn.Linear(hiddenDim, numRumorClass)
-        self.v1Stance = nn.Linear(hiddenDim, numStanceClass)
-        self.vStance = nn.Linear(hiddenDim, numStanceClass)
+        self.vRumor = nn.Linear(self.D * hiddenDim, numRumorClass)
+        self.vStance = nn.Linear(self.D * 2 * hiddenDim, numStanceClass) # stance的预测需要拼接第一句的隐状态
         
         # 训练集前向传递，返回对特定任务的概率向量/矩阵
     def forwardRumor(self, sentences: torch.Tensor):
         seqLen = sentences.size()[0]
-        sentence_len = sentences.size()[1]
-        embeddings = self.embeddingRumor(sentences).view(seqLen, self.batchSize, self.embeddingDim)
-        hs, _ = self.shareGRU(embeddings, self.h0)
-        gru_Rumor_in = embeddings + self.u(hs)
-        hs_Rumor, ht = self.GRURumor(gru_Rumor_in, self.h0Rumor)
-        ht = ht[ht.size()[0] - 1].view(self.hiddenDim)
+        embeddings = self.embeddingRumor(sentences).view(seqLen, self.batchSize, self.embeddingDim) # embeddings(sepLen, batch, embeddingDim)   
+        gruShareOut, _ = self.GRUshare(embeddings, self.h0)
+        # gruShareOut(seqLen, batch, numDirection * hiddenDim)
+        
+        hS2M = self.US2M(gruShareOut) # hS2M(sepLen, batch, embeddingDim)
+        gruRumorIn = embeddings + hS2M
+        gruRumorOut, _ = self.GRURumor(gruRumorIn, self.h0Rumor)
+        # gruRumorOut(seqLen, batch, numDirection * hiddenDim)
+
+        ht = gruRumorOut[gruRumorOut.size()[0] - 1].view(self.batchSize, self.D * self.hiddenDim) # 取出最后一层的隐状态
         p = self.vRumor(ht)
         return p
         
@@ -100,11 +111,16 @@ class MTES(nn.Module):
         seqLen = sentences.size()[0]
         sentence_len = sentences.size()[1]
         embeddings = self.embeddingRumor(sentences).view(seqLen, self.batchSize, self.embeddingDim)
-        hs, _ = self.shareGRU(embeddings, self.h0)
-        gru_Stance_in = embeddings + self.u(hs)
-        hs_Stance, ht = self.GRURumor(gru_Stance_in, self.h0Rumor)
-        hs_Stance = hs_Stance.view(seqLen, self.hiddenDim)
-        ps = self.v1Stance(hs_Stance[0]) + self.vStance(hs_Stance)
+        gruShareOut, _ = self.GRUshare(embeddings, self.h0)
+        # gruShareOut(seqLen, batch, numDirection * hiddenDim)
+        
+        hS2M = self.US2M(gruShareOut) # hS2M(sepLen, batch, embeddingDim)
+        gruStanceIn = embeddings + hS2M
+        gruStanceOut, _ = self.GRURumor(gruStanceIn, self.h0Rumor)
+        # gruRumorOut(seqLen, batch, numDirection * hiddenDim)
+
+        h1 = gruStanceOut[0].repeat(seqLen, 1, 1) # h1(seqLen, batch, numDirection * hiddenDim)
+        ps = self.vStance(torch.cat([h1, gruStanceOut], dim=2))
         return ps
         
     # 更换计算设备
@@ -112,3 +128,10 @@ class MTES(nn.Module):
         _model = self.to(device)
         _model.device = device
         return _model
+
+    # 保存模型
+    def save(self, path: str):
+        torch.save(self.state_dict(), path)
+    # 加载模型
+    def load(self, path: str):
+        self.load_state_dict(torch.load(path))
